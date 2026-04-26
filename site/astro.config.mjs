3ed7a91dbd30590f7b2545d718b9026f88b1e5ed
Greg <you@local>
2026-04-25 21:11:19 -0700

    feat(site): add Base layout, Footer, Disclaimer components

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
@@ -6,5 +10,14 @@ export default defineConfig({
   trailingSlash: "never",
   build: {
     format: "file",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@lib": path.resolve(__dirname, "src/lib"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@layouts": path.resolve(__dirname, "src/layouts"),
+      },
+    },
   },
 });
